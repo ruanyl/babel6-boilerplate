7adda437287e2e6be531b07ed82a4056a2398464
ruanyl <ruanyu1@gmail.com>
2017-02-22 16:55:25 +0200

    update/remove packages

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
-var test = require('tape');
+import test from 'ava'
 
-test('should be true', function(t) {
-  t.plan(1);
-  t.ok(true, 'true should be true');
-});
+test('should be true', t => {
+  t.plan(1)
+  t.true(true, 'true should be true')
+})
